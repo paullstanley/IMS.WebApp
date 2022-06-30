a8a51aeabc5bff3af7e1fa4970aa9fa0ae271c8a
Paull Stanley <paull.stanley@me.com>
2022-06-30 09:55:43 -0400

    Ability to add product to inventory
    - Added ability to create and add product to inventory.
    - Added price validation for products.

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using IMS.CoreBusiness.Validations;
 
 namespace IMS.CoreBusiness
 {
@@ -13,10 +14,26 @@ namespace IMS.CoreBusiness
         public int Quantity { get; set; }
 
         [Range(0, double.MaxValue, ErrorMessage = "Price must be greater or equal to {0}")]
+        [Product_EnsurePriceIsGreaterThanInventoriesPrice]
         public double Price { get; set; }
 
         public List<ProductInventory>? ProductInventories { get; set; }
 
+        public double TotalInventoryCost()
+        {
+            return this.ProductInventories.Sum(x => x.Inventory?.Price * x.InventoryQuantity ?? 0);
+        }
+
+
+        public bool ValidatePricing()
+        {
+            if (ProductInventories == null || ProductInventories.Count <= 0) return true;
+
+           
+           if (this.TotalInventoryCost() > Price) return false;
+
+            return true;
+        }
 	}
 }
 
---
+++ IMS.CoreBusiness/Validations/Product_EnsurePriceIsGreaterThanInventoriesPrice.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness.Validations
+{
+    internal class Product_EnsurePriceIsGreaterThanInventoriesPrice: ValidationAttribute
+	{
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var product = validationContext.ObjectInstance as Product;
+            if (product != null)
+            {
+                if (!product.ValidatePricing())
+                    return new ValidationResult($"The product's price is less than the sum of its inventories' price: {product.TotalInventoryCost() } !.",
+                        new[] { validationContext.MemberName });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
+
